d2f7976030e21ca78cc262fb66314a5afbd459f4
Ed <contatoedineybraga@gmail.com>
2022-09-02 23:51:59 -0300

    style-tag-range

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const range = document.querySelector("#range");
+const payMonth = document.querySelector("h2");
+
 
 range.addEventListener("input", () => {
-    let x = range.value;
-    let color = "linear-gradient(90deg, rgb(117,252,117)" + x + "%, rgb(214,214,214)" + x + "% )"; 
 
-    console.log(x)
+    let rangeValue = range.value;
+    let x = rangeValue * 3.125;
+    let color = "linear-gradient(90deg,rgb(16, 213, 194)" + x + "%, rgb(234, 238, 251)" + x + "% )";    payMonth.innerHTML = `<h2>$${rangeValue}.00</h2>`
+    console.log(x);
     range.style.background = color;
-})
+});
